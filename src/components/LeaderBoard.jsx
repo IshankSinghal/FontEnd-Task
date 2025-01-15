da290bd4786baa61bcea18196e2f1b870209df9c
Ishank Singhal <singhalishank004@gmail.com>
2025-01-15 22:50:15 +0530

    fix: bugs and final changes

--- src/components/LeaderBoard.jsx
+++ src/components/LeaderBoard.jsx
@@ -5,6 +5,7 @@ import TopTraders from "./TopTraders.jsx";
 import KeyMetrics from "./KeyMetrics.jsx";
 import FilterButton from "./Filterbutton.jsx";
 import LeaderboardTable from "./LeaderboardTable.jsx";
+import { Triangle } from "react-loader-spinner";
 
 function Leaderboard() {
   const [leaderboardData, setLeaderboardData] = useState({
@@ -58,7 +59,32 @@ function Leaderboard() {
 
   const filteredTraders = filterTraders();
 
-  if (isLoading) return <p>Getting the traders...</p>;
+  if (isLoading)
+    return (
+      <>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh", // Full viewport height
+            width: "100vw", // Full viewport width
+            backgroundColor: "#f8f8f8", // Optional background color
+          }}
+        >
+          <Triangle
+            visible={true}
+            height="200" // Larger height
+            width="200" // Larger width
+            color="#4fa94d"
+            ariaLabel="triangle-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+          />
+          <h1 className="font-semibold text-2xl">Getting the Traders...</h1>
+        </div>
+      </>
+    );
   if (fetchError) return <p>{fetchError}</p>;
 
   return (
