5b1f249edcd990218dd623ceb77ce4121fda3720
Ishank Singhal <singhalishank004@gmail.com>
2025-01-15 16:52:57 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 // src/App.js
-import React from 'react';
-import Leaderboard from './components/LeaderBoard';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Leaderboard from "./components/LeaderBoard";
 
 function App() {
   return (
-    <Leaderboard />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Leaderboard />} />
+        {/* You can add more routes here */}
+      </Routes>
+    </Router>
   );
 }
 
